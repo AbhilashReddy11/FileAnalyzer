fccc66d8ebb0dfa9b961738e54040ddfb92e6f3d
AbhilashReddy11 <yennapu.abhilash@kellton.com>
2023-07-19 11:34:43 +0530

    Add project files.

---
+++ FileAnalyzer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FileAnalyzer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ FileAnalyzer/Models/InputOutputModel.cs
@@ -0,0 +1,13 @@
+﻿// File: InputOutputModel.cs
+using System.ComponentModel.DataAnnotations;
+
+namespace FileAnalyzer.Models
+{
+    public class InputOutputModel
+    {
+        [Required]
+        public string Prompt { get; set; }
+        public string Response { get; set; }
+        public string ResponseFileUrl { get; set; }
+    }
+}
